099047e343b2af94a9d29bb96bfce8387f7c1541
Samuel Lima <samuel.aguiar0106@gmail.com>
2026-07-01 12:03:00 -0300

    Ajustes finais

--- Enviar.py
+++ Enviar.py
@@ -1,3 +1,4 @@
+#IMPORTAÇÃO DAS BIBLIOTECAS
 from Leitura import leitura, data_atual
 import smtplib
 import os
@@ -5,16 +6,14 @@ from dotenv import load_dotenv
 from email.mime.text import MIMEText
 from datetime import datetime, timezone, timedelta
 
+#DEFINIÇÃO DA HORA EM BRASILIA A CONCIDERAR O ATRASO DE 3 HORAS
 fuso_brasilia = timezone(timedelta(hours=-3))
 hora_atual = datetime.now(fuso_brasilia).strftime("%H:%M")
 
 if os.path.exists(".env"):
     load_dotenv()
 
-remetente = os.environ.get("EMAIL_USER")
-senha = os.environ.get("EMAIL_PASS")
-destinatario = "samuel.aguiar0106@gmail.com"
-
+#FUNÇÃO QUE RECEBE A LISTA E LE E CRIA UMA MENSAGEM
 def mensagem_enviar(parametro_tarefas):
     if not parametro_tarefas:
         return "Você não tem compromissos agendados para hoje! Aproveite o dia."
@@ -24,15 +23,21 @@ def mensagem_enviar(parametro_tarefas):
         mensagem_str += f"{mensagem} \n"
     return mensagem_str
 
+#LÊ AS INFORMAÇÕES DE OUTRA ARQUIVO E DEFINE A MENSAGEM
 lista_de_hoje = leitura()
 mensagem_final = mensagem_enviar(lista_de_hoje)
 print(mensagem_final)
 
+#DEFINIÇÃO DE VARIÁVEIS PARA ENVIO DE E-MAIL
+remetente = os.environ.get("EMAIL_USER")
+senha = os.environ.get("EMAIL_PASS")
+destinatario = "samuel.aguiar0106@gmail.com"
 msg = MIMEText(f"{mensagem_final}", "plain")
 msg["From"] = remetente
 msg["To"] = destinatario
 msg["Subject"] = f"Cronograma de Hoje - {data_atual.capitalize()}, horário: {hora_atual}"
 
+#ENVIA O E-MAIL
 with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
     server.login(remetente, senha)
     server.send_message(msg)
